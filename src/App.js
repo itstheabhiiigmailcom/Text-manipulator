8596322f4ada4fba8aabe2c8c7931af6ec159152
itstheabhiiigmailcom <154773883+itstheabhiiigmailcom@users.noreply.github.com>
2025-03-23 18:22:36 +0530

    convert to camel case functionality added

--- src/App.js
+++ src/App.js
@@ -4,66 +4,71 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React, {useState} from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   //usestate to set the dark Mode
-  const [mode, setMode] = useState('light');      //whether dark mode is enabled or not
+  const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
   //usestate to give an alert
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type 
-      })
-      setTimeout(()=>{            // setting timeout to dismiss the alert automatically after 1500 ms
-        setAlert(null);
-      },1500);
-  }
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      // setting timeout to dismiss the alert automatically after 1500 ms
+      setAlert(null);
+    }, 1500);
+  };
 
-  const toggleMode = ()=>{
+  const toggleMode = () => {
     // removeBodyClasses();
     // console.log(cls);
     // document.body.classList.add('bg-'+cls);
-      if(mode === 'light'){
-        setMode('dark');
-        document.body.style.backgroundColor='#042743';
-        showAlert("Dark mode has beeen Enabled","success");
-        // document.title = "TextUtils - Dark Mode";        // To change the title, when goes from dark to light mode
-        // setInterval(()=>{
-        //   document.title = "TextUtils is Amazing";
-        // },2000)
-        // setInterval(()=>{
-        //   document.title = "Install TextUtils Now";
-        // },1500)
-      }
-      else{
-        setMode('light');
-        document.body.style.backgroundColor='white';
-        showAlert("Light mode has been enabled", "success");
-        document.title = "TextUtils - Light Mode";
-      }
-  }
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert('Dark mode has beeen Enabled', 'success');
+      // document.title = "TextUtils - Dark Mode";        // To change the title, when goes from dark to light mode
+      // setInterval(()=>{
+      //   document.title = "TextUtils is Amazing";
+      // },2000)
+      // setInterval(()=>{
+      //   document.title = "Install TextUtils Now";
+      // },1500)
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light mode has been enabled', 'success');
+      document.title = 'TextUtils - Light Mode';
+    }
+  };
 
   return (
     <>
-    <Router>
-      <Navbar title="Textutils1" mode={mode} toggleMode = {toggleMode}/>
-      <Alert alert={alert}/>
-      <div className="container my-3">
-      <Routes>
-        <Route exact path="/about" element={<About mode={mode}/>}></Route>
-        <Route exact path="/" element={<TextForm heading="Try TextUtils - Word counter, Character counter, 
-        Remove Extra spaces" mode={mode} showAlert={showAlert}/>}></Route>
-      </Routes>
-      </div>
+      <Router>
+        <Navbar title="Textutils1" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About mode={mode} />}></Route>
+            <Route
+              exact
+              path="/"
+              element={
+                <TextForm
+                  heading="Try TextUtils - Word counter, Character counter, 
+        Remove Extra spaces"
+                  mode={mode}
+                  showAlert={showAlert}
+                />
+              }
+            ></Route>
+          </Routes>
+        </div>
       </Router>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,57 +1,131 @@
-import React, { useState } from 'react'
-
+import React, { useState } from 'react';
 
 export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert('Converted to Uppercase!', 'success');
+  };
 
-    const handelUpClick = () => {
-        let newText = text.toUpperCase();
-        setText(newText);
-        props.showAlert("Converted to Uppercase!", "success");
-    }
-    const handelLowClick = () => {
-        let newText = text.toLowerCase();
-        setText(newText);
-        props.showAlert("Converted to Lowercase!", "success");
-    }
-    const handelClearClick = ()=>{
-        let newText = '';
-        setText(newText);
-        props.showAlert("Text cleared!", "success");
-    }
-    const handelCopy = ()=>{
-        navigator.clipboard.writeText(text);
-        props.showAlert("copied to clipboard", "success");
-    }
-    const handelExtraSpaces = ()=>{
-        var newText = text.split(/[ ]+/);
-        setText(newText.join(" "));
-        props.showAlert("Extraspaces removed!", "success");
-    }
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert('Converted to Lowercase!', 'success');
+  };
 
-    const handelOnChange = (event) => {
-        setText(event.target.value);                    // textarea me kuchh bhi type krne par phir se vo text k equal ho jana chahiye tnhi likh payenge text area me
-    }
-    const [text, setText] = useState('');
-    return (
-        <>
-            <div className='container' style={{color: props.mode ==='dark'?'white':'black'}}>
-                <h2 className='mb-4'>{props.heading}</h2>
-                <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handelOnChange} style={{backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', color: props.mode==='dark'?'white':'black'}} rows="8" id="myBox"></textarea>
-                </div>
-                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handelUpClick}>Convert to Uppercase</button>      {/* Here we have use the disabled command to disable buttons when there is nothing in the box */}
-                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handelLowClick}>Convert to Lowercase</button>
-                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handelClearClick}>Clear</button>
-                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handelCopy}>Copy Text</button>
-                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handelExtraSpaces}>Remove Extra Spaces</button>
-            </div>
-            <div className="container my-2" style={{color: props.mode ==='dark'?'white':'black'}}>
-                <h1>Your Text summary</h1>
-                <p>{text.split(/\s+/).filter((element)=>{return element.length !== 0}).length} words and {text.length} characters</p>
-                <p>{0.008 * text.split(" ").filter((element)=>{return element.length !== 0}).length} Minutes to read</p>
-                <h3>Preview</h3>
-                <p>{text.length>0?text:"Nothing to Preview"}</p>
-            </div>
-        </>
-    )
+  const handleClearClick = () => {
+    setText('');
+    props.showAlert('Text cleared!', 'success');
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    props.showAlert('Copied to clipboard', 'success');
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/).join(' ');
+    setText(newText);
+    props.showAlert('Extra spaces removed!', 'success');
+  };
+
+  const handleCamelCaseClick = () => {
+    let words = text.toLowerCase().split(/\s+/);
+    let camelCaseText = words
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' '); // Join words with spaces
+    setText(camelCaseText);
+    props.showAlert('Converted to Camel Case!', 'success');
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState('');
+
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === 'dark' ? 'white' : 'black' }}
+      >
+        <h2 className="mb-4">{props.heading}</h2>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor:
+                props.mode === 'dark' ? 'rgb(36 74 104)' : 'white',
+              color: props.mode === 'dark' ? 'white' : 'black',
+            }}
+            rows="8"
+            id="myBox"
+          ></textarea>
+        </div>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-2"
+          onClick={handleUpClick}
+        >
+          Convert to Uppercase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-2"
+          onClick={handleLowClick}
+        >
+          Convert to Lowercase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-2"
+          onClick={handleClearClick}
+        >
+          Clear
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-2"
+          onClick={handleCopy}
+        >
+          Copy Text
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-2"
+          onClick={handleExtraSpaces}
+        >
+          Remove Extra Spaces
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-2"
+          onClick={handleCamelCaseClick}
+        >
+          Convert to Camel Case
+        </button>
+      </div>
+      <div
+        className="container my-2"
+        style={{ color: props.mode === 'dark' ? 'white' : 'black' }}
+      >
+        <h1>Your Text Summary</h1>
+        <p>
+          {text.split(/\s+/).filter((element) => element.length !== 0).length}{' '}
+          words and {text.length} characters
+        </p>
+        <p>
+          {0.008 *
+            text.split(' ').filter((element) => element.length !== 0)
+              .length}{' '}
+          Minutes to read
+        </p>
+        <h3>Preview</h3>
+        <p>{text.length > 0 ? text : 'Nothing to Preview'}</p>
+      </div>
+    </>
+  );
 }
